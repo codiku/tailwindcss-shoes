b10ab03f5920b288543067a2f706b8c5597800af
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-09-22 13:17:20 +0800

    wip

--- src/features/Nav.jsx
+++ src/features/Nav.jsx
@@ -5,11 +5,12 @@ import NikeLogo from "../assets/nike.svg?react";
 
 export function Nav() {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
+
   const buttonBurger = (
     <button
       onClick={() => setShowMobileMenu((prev) => !prev)}
       type="button"
-      className=" p-2 w-10 h-10  text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+      className=" p-2 w-10 h-10 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
     >
       <RxHamburgerMenu size={"auto"} />
     </button>
@@ -23,6 +24,7 @@ export function Nav() {
     >
       <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
+          {/* An inline element only takes up as much width as necessary */}
           <a
             href="#"
             className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
@@ -67,16 +69,24 @@ export function Nav() {
     </div>
   );
 
+  const buttonShoping = (
+    <div className="cursor-pointer hidden md:flex justify-center items-center rounded-full h-12 w-12 bg-white shadow-sm ">
+      <TbShoppingBag size={15} />
+    </div>
+  );
+
+  const logoLink = (
+    <a href="#" className="flex items-center">
+      <NikeLogo className="h-16 w-16" />
+    </a>
+  );
+
   return (
     <nav className="flex flex-wrap items-center justify-between px-8 md:px-32 py-8">
-      <a href="#" className="flex items-center">
-        <NikeLogo className="h-16 w-16" />
-      </a>
+      {logoLink}
       {buttonBurger}
       {menuList}
-      <div className="cursor-pointer hidden md:flex justify-center items-center rounded-full h-12 w-12 bg-white shadow-sm ">
-        <TbShoppingBag size={15} />
-      </div>
+      {buttonShoping}
     </nav>
   );
 }
