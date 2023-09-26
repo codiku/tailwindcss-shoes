d62782a476e2822453c9a6f510bca17db34891cc
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-09-26 20:05:10 +0800

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Nav } from "./components/Nav";
-
 import { useState } from "react";
 import { SHOE_LIST } from "./constant";
 import { NewArrivalSection } from "./features/NewArrivalSection";
