989d8c0b3df0f41e1d955f9653f2e18344ad42b9
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-10-16 18:10:25 +0200

    wip

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
-import "@fontsource-variable/nunito-sans";
-import "./global.css";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { App } from "./App.jsx";
+import "./index.css";
+
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: ["class"],
   theme: {
-    extend: {
-      colors: {
-        night: {
-          DEFAULT: "#0D1120",
-          50: "#171E2C",
-          500: "#0D1120",
-        },
-        paleYellow: "#EEFFA4",
-        lavender: "#DDCEFD",
-        paleGreen: "#DAFFA2",
-      },
-      keyframes: {
-        float: {
-          "0%, 100%": { transform: "translateY(0)" },
-          "50%": { transform: "translateY(-10px)" }, // Adjust the floating height as needed
-        },
-        fadeIn: {
-          "0%": { opacity: "0" },
-          "100%": { opacity: "1" },
-        },
-      },
-      animation: {
-        float: "float 4s infinite", // Adjust the duration as needed
-        fadeIn: "fadeIn 1s ease-in-out ",
-      },
-    },
+    extend: {},
   },
+  plugins: [],
 };
