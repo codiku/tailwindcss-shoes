9654b7f70bbed1b15dea14b02984bf93a672f3da
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-10-17 14:01:14 +0200

    wip

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,36 +1,43 @@
 import NikeLogo from "../assets/nike-logo.svg?react";
 import { RxHamburgerMenu } from "react-icons/rx";
 
-const ROUTES = ["Home", "About", "Services", "Pricing", "Contact"];
-
+const ROUTES = [
+  "Home",
+  "About",
+  "Services",
+  "Pricing",
+  "Contact",
+];
 export function Nav() {
   return (
     <nav className="flex flex-wrap items-center justify-between">
       {/* Logo */}
-      <a href="#" className="flex items-center">
+      <a href="#">
         <NikeLogo className="h-20 w-20" />
       </a>
 
       {/* Burger button */}
-      <button className="h-10 w-10 rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200">
+      <button className="rounded-lg p-2 hover:bg-gray-100 focus:ring-2 focus:ring-gray-200">
         <RxHamburgerMenu size={25} />
       </button>
 
       {/* Menu list */}
-      <div className={` mb-4 w-full`}>
-        <ul className="flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 text-lg ">
-          {ROUTES.map((route, i) => (
-            <li key={route}>
-              <a
-                href="#"
-                className={`${i == 0 && "bg-blue-500 text-white "}
-              ${i > 0 && "hover:bg-gray-100"}
-              block rounded px-3 py-2`}
+      <div className="w-full">
+        <ul className="rounded-lg border border-gray-100 bg-gray-50 p-4 text-lg">
+          {ROUTES.map((route, i) => {
+            return (
+              <li
+                className={`cursor-pointer rounded px-3 py-2 ${
+                  i === 0
+                    ? "bg-blue-500 text-white"
+                    : "hover:bg-gray-100"
+                }`}
+                key={route}
               >
-                {route}
-              </a>
-            </li>
-          ))}
+                <a>{route}</a>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </nav>
