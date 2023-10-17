324337b98f483f264585866e483db111af2a26aa
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-10-17 10:53:49 +0200

    wip

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -13,11 +13,11 @@ export function Nav() {
 
       {/* Burger button */}
       <button className="h-10 w-10 rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200">
-        <RxHamburgerMenu size={"auto"} />
+        <RxHamburgerMenu size={25} />
       </button>
 
       {/* Menu list */}
-      <div className={`mb-4 w-full`}>
+      <div className={` mb-4 w-full`}>
         <ul className="flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 text-lg ">
           {ROUTES.map((route, i) => (
             <li key={route}>
