e22d47b3cdd7d8df3c4e8f83124d66516045c31c
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-10-18 13:29:56 +0200

    wip

--- src/components/ShoeDetail.jsx
+++ src/components/ShoeDetail.jsx
@@ -1,15 +1,14 @@
 import nike1 from "../assets/n1-min.png";
+
 export function ShoeDetail() {
   return (
-    <div className="flex flex-col lg:flex-row-reverse">
-      {/* ml-28 to make it align with the menu */}
-      <div className="flex-1">
+    <div className="flex flex-col space-y-4 lg:flex-row-reverse">
+      <div className="flex-1 ">
         <img src={nike1} />
       </div>
-
       <div className="flex-1 space-y-6">
         <div className="text-5xl font-black md:text-9xl">
-          Nike air max 270
+          Nike Air max 270
         </div>
         <div className="font-medium md:text-xl">
           {
@@ -19,13 +18,10 @@ export function ShoeDetail() {
         <div className="text-3xl font-extrabold md:text-6xl">
           100 $
         </div>
-        <div className="flex items-center space-x-10">
-          <button
-            className={`flex-center  h-14 w-44 bg-black  text-white hover:bg-gray-900 active:bg-gray-700 `}
-          >
+        <div className="space-x-10">
+          <button className="h-14 w-44 bg-black text-white hover:bg-gray-900 active:bg-gray-700">
             Add to bag
           </button>
-
           <a
             href="#"
             className="text-lg font-bold underline underline-offset-4"
