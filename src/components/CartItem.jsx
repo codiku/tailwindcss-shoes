ccc86e2a1a523d9b570099cf58bd31b71d8ae2ac
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-10-19 18:27:39 +0200

    wip

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,44 +1,37 @@
-import { Select } from "./Select";
 import { CiTrash } from "react-icons/ci";
-import { QTY, SIZES } from "../constant";
-
-export function CartItem({ item, qty, size }) {
+import { Select } from "../components/Select";
+import { SIZES, QTY } from "../constant";
+export function CartItem({ item }) {
   return (
-    <div className=" cursor-pointer bg-gray-50 p-2 hover:bg-[#DAFFA2]">
-      <div>
-        <div className="flex  space-x-2  ">
-          {/* Image */}
-          <img className="h-24" src={item.src} />
-          {/* Title and description */}
-          <div className="space-y-2">
-            <div className="font-bold">{item.title}</div>
-            <div className="text-sm text-gray-400">
-              {item.description}
-            </div>
+    <div className="cursor-pointer space-y-2 bg-gray-50 p-2 hover:bg-[#DAFFA2]">
+      <div className="flex  space-x-2 ">
+        {/* Image */}
+        <img className="h-24" src={item.src} />
+        <div className="space-y-2">
+          {/* Title & Description */}
+          <div className="font-bold">{item.title}</div>
+          <div className="text-sm text-gray-400">
+            {item.description}
           </div>
-          {/* price */}
-          <div className="font-bold">{item.price}$</div>
         </div>
+        {/* Price */}
+        <div className="font-bold">{item.price}$</div>
+      </div>
 
-        <div className="flex justify-between gap-2 pl-32">
-          {/* Inputs */}
-          <div className="flex space-x-6">
-            {/* QTY input */}
-            <div className="mt-2 space-y-1">
-              <div className="font-bold">QTY</div>
-              <Select title={qty} options={QTY} />
-            </div>
-            {/* SIZE input */}
-            <div className="mt-2 space-y-1">
-              <div className="font-bold">SIZE</div>
-              <Select title={size} options={SIZES} />
-            </div>
+      <div className="flex justify-between pl-32">
+        <div className="flex space-x-6">
+          <div>
+            <div className="font-bold">SIZE</div>
+            <Select title="" options={SIZES} />
           </div>
-          {/* Trash icon */}
-          <button className="flex-center mt-6">
-            <CiTrash size={25} className="text-black" />
-          </button>
+          <div>
+            <div className="font-bold">QTY</div>
+            <Select title="" options={QTY} />
+          </div>
         </div>
+        <button>
+          <CiTrash size={25} className="text-black" />
+        </button>
       </div>
     </div>
   );
