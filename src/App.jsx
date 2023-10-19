b271f00ea896fee1e022bd2a17a4f9f7096b2520
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-10-19 17:18:03 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ export function App() {
         isOpen={isSidebarOpen}
         onClickClose={() => setIsSidebarOpen(false)}
       >
-        Hello
+        Hi
       </Sidebar>
     </div>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,23 +1,22 @@
-export const Sidebar = ({ isOpen, onClickClose, children }) => {
+export function Sidebar({ children, isOpen, onClickClose }) {
   return (
-    <>
+    <div>
       <div
-        className={`fixed right-0 top-0 z-50 h-full w-full transform overflow-auto bg-white p-5 shadow-lg transition duration-300 md:w-[50%] lg:w-[35%]  ${
+        className={`fixed right-0 top-0 z-50 h-full w-full transform bg-white p-5 shadow-lg transition duration-300 md:w-[50%] lg:w-[35%] ${
           isOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
         <button
           onClick={onClickClose}
-          className="absolute right-4 top-4  rounded-full p-2 text-black "
+          className="absolute right-4 top-4 p-2 font-bold text-black"
         >
           X
         </button>
         {children}
       </div>
-      {/*Overlay*/}
       {isOpen && (
         <div className="fixed left-0 top-0 z-20 h-full w-full bg-black opacity-50" />
       )}
-    </>
+    </div>
   );
-};
+}
