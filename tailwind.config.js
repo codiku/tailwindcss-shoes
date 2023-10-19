beb17254c51f6c6454de2ecf2848cc1943ccf929
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-10-19 11:27:32 +0200

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,28 +4,23 @@ export default {
   theme: {
     extend: {
       keyframes: {
-        press: {
-          "0%": { transform: "scale(1)" },
-          "100%": { transform: "scale(0.9)" },
+        wiggle: {
+          "0%,100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+        float: {
+          "0%, 100%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(-10px)" },
         },
         fadeIn: {
           "0%": { opacity: "0" },
           "100%": { opacity: "1" },
-        },
-        float: {
-          "0%, 100%": { transform: "translateY(0)" },
-          "50%": { transform: "translateY(-10px)" }, // Adjust the floating height as needed
-        },
-        wiggle: {
-          "0%, 100%": { transform: "rotate(-3deg)" },
-          "50%": { transform: "rotate(3deg)" },
         },
       },
       animation: {
         wiggle: "wiggle 1s ease-in-out infinite",
         float: "float 4s ease-in-out infinite",
         fadeIn: "fadeIn 1s ease-in-out",
-        press: "press 0.5s",
       },
     },
   },
