4c923a31905269d9d660ee9497135867355f9672
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-10-19 14:47:05 +0200

    wip

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,12 +5,12 @@ export function Card({ item }) {
     >
       <div className="p-8">
         <div className="text-2xl font-bold">{item.title}</div>
-        <div className=" text-md mt-10 font-semibold underline underline-offset-4">
+        <div className="mt-10 font-semibold underline underline-offset-4">
           SHOP NOW +
         </div>
       </div>
       <img
-        className="absolute left-[40%] top-5  h-40 w-56  "
+        className="absolute left-[40%] top-5 h-40 w-56"
         src={item.src}
       />
     </div>
